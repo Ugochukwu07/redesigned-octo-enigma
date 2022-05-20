e228a6f4a681401ed919c5630f2c69ef6536e407
Ekwueme Ugochukwu <ekwuemeugochukwu83@gmail.com>
2022-05-20 02:45:02 +0100

    made html

--- resources/views/mails/contact/reply.blade.php
+++ resources/views/mails/contact/reply.blade.php
@@ -3,6 +3,6 @@
 @section('content')
 <div class="v-text-align v-line-height" style="line-height: 140%; text-align: left; word-wrap: break-word;">
     <p>Hello, {{ $message->name }}</p>
-    {{ $message->reply }}
+    {!! $message->reply !!}
   </div>
 @endsection
